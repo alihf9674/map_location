4fe5bbed93b6d21763053efd965e7089cd953e60
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-07-10 14:20:28 +0330

    Add project templates

---
+++ adm.php
@@ -0,0 +1,14 @@
+<?php
+include 'bootstrap/init.php';
+if (isset($_GET['logout']) && $_GET['logout'] == 1)
+    logout();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!login($_POST['username'], $_POST['password']))
+        message('نام کاربری یا رمز عبور اشتباه است.');
+}
+
+if (isLoggedIn())
+    include 'template/admin-panel.php';
+else
+    include 'template/auth.php';
